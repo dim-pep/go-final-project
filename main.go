ef7b31638a5945d045fd4d24614135d70e5d7b33
Вязовский Дмитрий Николаевич <Pepidastr@yandex.ru>
2025-05-27 18:51:25 +0500

    Создание скрипта для открытия/создания БД(2 Шаг)

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "go1f/pkg/server"
+import (
+	"go1f/pkg/db"
+	"go1f/pkg/server"
+	"log"
+)
 
 func main() {
-	server.Start()
+	dbFile := "scheduler.db"
+
+	if err := db.Init(dbFile); err != nil {
+		log.Fatalf("Ошибка инициализации базы данных: %v", err)
+	}
+
+	if err := server.Start(); err != nil {
+		log.Fatalf("Ошибка запуска веб-сервера: %v", err)
+	}
 }
---
+++ pkg/db/db.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "modernc.org/sqlite"
+)
+
+var db *sql.DB
+
+const (
+	schema = `
+CREATE TABLE IF NOT EXISTS scheduler (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date CHAR(8) NOT NULL DEFAULT "",
+    title VARCHAR(255) NOT NULL,
+    comment TEXT,
+    repeat VARCHAR(128)
+);
+CREATE INDEX IF NOT EXISTS idx_date ON scheduler(date);
+`
+)
+
+func Init(dbFile string) error {
+
+	db, err := sql.Open("sqlite", dbFile)
+	if err != nil {
+		return fmt.Errorf("ошибка открытия базы данных: %v", err)
+	}
+
+	_, err = db.Exec(schema)
+	if err != nil {
+		return fmt.Errorf("ошибка создания таблицы: %v", err)
+	}
+
+	return nil
+}
